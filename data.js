65faf88363c7f3e7cad848433ab7bca82b4d0811
Grzegorz Faszcza <32841516+QkiPlay@users.noreply.github.com>
2025-09-25 19:31:46 +0200

    Brak zmian

--- data.js
+++ data.js
@@ -1,13 +1,8 @@
-// Edit this file to manage files and folders!
-// Use null for files, object for folders.
-
 window.fileStructure = {
     "README.md": null,
     "Wprowadzenie do informatyki": {
         "Python": {
-            "Zadanie 9.1.py": null,
-            "subsubfolder": {
-            }
+            "Zadanie 9.1.py": null
         }
     }
 };
