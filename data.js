a176ce4420ed78357a16c29c96f7307b8eb4464f
Grzegorz Faszcza <32841516+QkiPlay@users.noreply.github.com>
2025-09-25 18:54:45 +0200

    Struktura plików w oddzielnym pliku

---
+++ data.js
@@ -0,0 +1,13 @@
+// Edit this file to manage files and folders!
+// Use null for files, object for folders.
+
+window.fileStructure = {
+    "README.md": null,
+    "Wprowadzenie do informatyki": {
+        "Python": {
+            "Zadanie 9.1.py": null,
+            "subsubfolder": {
+            }
+        }
+    }
+};
